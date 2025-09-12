d96c13b8b096a621d951ed7332ffebed5feda417
maronect <maronect@gmail.com>
2025-09-12 23:33:51 +0000

    comment: describes main Markowitz's functions

--- src/data/loader.py
+++ src/data/loader.py
@@ -2,8 +2,15 @@ import yfinance as yf
 import pandas as pd
 
 def load_prices(tickers, start, end):
+    '''
+    carrega as cotações de fechamento de cada dia ajustadas
+    '''
     df = yf.download(tickers, start=start, end=end)['Close']
     return df.dropna()
 
-def compute_returns(prices):
-    return prices.pct_change().dropna()
+def compute_returns(prices): 
+    '''
+    Variação percentual entre um dia e o anterior.
+    dropna remove os valores NaN que aparecem no primeiro dia.
+    '''
+    return prices.pct_change().dropna() 
--- src/optimization/markowitz.py
+++ src/optimization/markowitz.py
@@ -2,19 +2,29 @@ from scipy.optimize import minimize
 import numpy as np
 
 def portfolio_return(weights, mean_returns):
+    '''
+    Retorno esperado do portfólio
+    '''
     return np.dot(weights, mean_returns)
 
 def portfolio_volatility(weights, cov_matrix):
+    '''
+    Cálculo do risco (volatilidade) do portfólio (com base na matriz de covariância entre os ativos)
+    '''
     return np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))
 
 def minimize_volatility(mean_returns, cov_matrix, target_return):
+    '''
+    Busca a combinação de ativos (pesos) que atinge um retorno esperado pré-determinado e minimiza 
+    a volatilidade, respeitando as restrições
+    '''
     num_assets = len(mean_returns)
     constraints = [
-        {'type': 'eq', 'fun': lambda x: np.sum(x) - 1},
-        {'type': 'eq', 'fun': lambda x: portfolio_return(x, mean_returns) - target_return}
+        {'type': 'eq', 'fun': lambda x: np.sum(x) - 1}, #soma dos pesos = 1
+        {'type': 'eq', 'fun': lambda x: portfolio_return(x, mean_returns) - target_return} # retorno alvo do portoflio deve ser = ao escolhido
     ]
-    bounds = tuple((0, 1) for _ in range(num_assets))
-    initial_guess = num_assets * [1. / num_assets]
+    bounds = tuple((0, 1) for _ in range(num_assets)) # pesos devem ser valores entre 0 e 1
+    initial_guess = num_assets * [1. / num_assets] # a principio mesmo peso para todos
     result = minimize(portfolio_volatility, initial_guess, args=(cov_matrix,),
                       method='SLSQP', bounds=bounds, constraints=constraints)
     return result.x if result.success else None
