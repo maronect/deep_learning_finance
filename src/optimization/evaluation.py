48592d8a60222b6dc5135a89a06156897a6b9499
maronect <maronect@gmail.com>
2025-11-13 01:13:03 +0000

    compare lr with standard mk

--- src/optimization/evaluation.py
+++ src/optimization/evaluation.py
@@ -4,11 +4,12 @@ import numpy as np
 def evaluate_portfolio(returns: pd.DataFrame, weights: np.ndarray, freq='daily', model_name='baseline'):
     """
     Calcula métricas de desempenho de um portfólio.
+    não está avaliando o modelo de ML diretament, mas sim o resultado econômico da decisão que ele formulou.
     """
     portfolio_returns = returns.dot(weights)
     mean = portfolio_returns.mean()
     vol = portfolio_returns.std()
-    sharpe = mean / vol * np.sqrt(252 if freq == 'daily' else 12)
+    sharpe = mean / vol * np.sqrt(252 if freq == 'daily' else 12) # unidades de retorno do portifolio por unidade de risco, nromalmente entre 1 e 2. acima disso é crazy
 
     return {
         "Model": model_name,
